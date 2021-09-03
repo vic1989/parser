9ab315c831a994c2ae0f6d09862f270b32096f9e
vic <xxx.zero@mail.ru>
2021-09-03 18:50:21 +0300

    initial

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 module.exports = {
     connect: () => {
         return new Promise((resolve) => {
-            mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true, useUnifiedTopology: true}).catch((err) => {
+            mongoose.connect('mongodb://localhost:27017/parser', {useNewUrlParser: true, useUnifiedTopology: true}).catch((err) => {
                 console.error("\x1b[41m", 'db connection error:' + err)
                 process.exit(1);
             });
